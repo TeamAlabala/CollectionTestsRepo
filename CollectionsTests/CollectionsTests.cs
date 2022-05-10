24272e9fda105f0367fb44d9108ccdbee599950b
Nikolay Kolev <nikolay.kolev1989@gmail.com>
2022-05-10 19:35:29 +0300

    Removed empty lines

--- CollectionsTests/CollectionsTests.cs
+++ CollectionsTests/CollectionsTests.cs
@@ -33,7 +33,7 @@ namespace CollectionsTests
             var expected = new Collections.Collection<int>(5, 10, 15, 20, 25, 30);
             Assert.AreEqual(expected.ToString(), myValues.ToString(), $"The new collection is:{myValues.ToString()}");
             Trace.WriteLine($"The new collection is:{myValues.ToString()}");
-        }
+        }      
         
          [Test]
         public void ExchangeFirstLast()
@@ -41,15 +41,12 @@ namespace CollectionsTests
             //arrange
             var numsToTest = new Collection<int>(1, 2, 3, 4, 5);
             var numsExpected = new Collection<int>(5, 2, 3, 4, 1);
-
             //act
             numsToTest.Exchange(0, 4) ;
-
             //assert
             Assert.AreEqual(numsToTest[0], numsExpected[0]);
-
         }
-
+        
         [Test]
         public void ExchangeMiddleIndexes()
         {
@@ -62,7 +59,7 @@ namespace CollectionsTests
 
             //assert
             Assert.AreEqual(numsToTest[2], numsExpected[2]);
-        }
+        }        
         
         [Test]
         public void GetByIndex()
@@ -70,71 +67,49 @@ namespace CollectionsTests
             //arrange
             var numsToTest = new Collection<int>(1, 2, 3, 4, 5);
             int numByIndex = 4;
-
             //act
             int testedNum = numsToTest[3];
-
             //assert
             Assert.That(testedNum == numByIndex);
         }
-
+        
         [Test]
         public void InsertAtEnd()
         {
             //arrange
             var numsToTest = new Collection<int>(1, 2, 3, 4, 5);
             int numToInsert = 6;
-
             //act
             numsToTest.Add(numToInsert);
-
             //assert
             Assert.That(numsToTest[numsToTest.Count-1] == numToInsert);
-        }
+        }  
         
         [Test]
         public void ExchangeInvalidIndexes()
         {
             //arrange
             var numsToTest = new Collection<int>(1, 2, 3, 4, 5);
-
-            //act
-            //numsToTest.Exchange(0, 6);
-
             //assert
-
             Assert.That(() => numsToTest.Exchange(0, 6), Throws.InstanceOf<System.ArgumentOutOfRangeException>());
-
         }
-
+        
         [Test]
         public void GetByInvalidIndex()
         {
             //arrange
             var numsToTest = new Collection<int>(1, 2, 3, 4, 5);
-
-            //act
-            //numsToTest.Exchange(0, 6);
-
             //assert
-
             Assert.That(() => numsToTest[15], Throws.InstanceOf<System.ArgumentOutOfRangeException>());
-
         }
-
+        
         [Test]
         public void InsertByInvalidIndex()
         {
             //arrange
-            var numsToTest = new Collection<int>(1, 2, 3, 4, 5);
-
-            //act
-            //numsToTest.Exchange(0, 6);
-
+            var numsToTest = new Collection<int>(1, 2, 3, 4, 5);    
             //assert
-
             Assert.That(() => numsToTest.InsertAt(15, 6), Throws.InstanceOf<System.ArgumentOutOfRangeException>());
-
         }
     }
 }
