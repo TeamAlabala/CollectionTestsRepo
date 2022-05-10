ef1275715c5905edc8eb547da41a5e3642d26071
gerganasuv <gerganasuv@gmail.com>
2022-05-10 20:19:35 +0300

    Small Fix

--- CollectionsTests/CollectionsTests.cs
+++ CollectionsTests/CollectionsTests.cs
@@ -1,3 +1,4 @@
+using Collections;
 using NUnit.Framework;
 using System.Diagnostics;
 
@@ -10,7 +11,7 @@ namespace CollectionsTests
         public void Test_Collections_1MillionItems()
         {
             //Arrange
-            var myValues = new Collections.Collection<int>(new int[1000000]);
+            var myValues = new Collection<int>(new int[1000000]);
             var expected = myValues.Count;
 
             //Act
@@ -24,13 +25,13 @@ namespace CollectionsTests
         public void Test_Collections_Add()
         {
             //Arrange
-            var myValues = new Collections.Collection<int>(5, 10, 15, 20, 25);
+            var myValues = new Collection<int>(5, 10, 15, 20, 25);
 
             //Act
             myValues.Add(30);
 
             //Assert
-            var expected = new Collections.Collection<int>(5, 10, 15, 20, 25, 30);
+            var expected = new Collection<int>(5, 10, 15, 20, 25, 30);
             Assert.AreEqual(expected.ToString(), myValues.ToString(), $"The new collection is:{myValues.ToString()}");
             Trace.WriteLine($"The new collection is:{myValues.ToString()}");
         }      
